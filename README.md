7c2ddfa726cc6761a9eb49d8694eed5a7c8ef648
Alex Cho <alexsjcho@gmail.com>
2019-02-06 15:55:08 -0800

    loaded previous app filse

---
+++ README.md
@@ -0,0 +1,18 @@
+#Assignment
+
+#App 3:
+
+<!-- 1. Create an app that lets users choose to display between 1 and 50 random dog images, then prints the results to the console. [done]
+
+2. The app should feature a form with a required input where users indicate the number of images to retrieve, and the input should default to 3. [done]
+
+3. Use the endpoint described in the "DISPLAY MULTIPLE RANDOM IMAGES FROM ALL DOGS COLLECTION" section of this page of the DogAPI docs.[done] -->
+<!-- 
+1. Building on the previous app, create an app that lets users choose to display between 1 and 50 random dog images, then loads the images in the console. [done]
+
+2. This app should adhere to all of the requirements from the first one, in addition to displaying the images in the DOM. [done] -->
+
+1. Create an app that loads a single random image for a specific breed. [done]
+2. This app should account for the happy case when the breed is found, as well as the unhappy case when it is not. 
+3. Use the endpoint described in the "RANDOM IMAGE FROM A BREED COLLECTION" section of this page of the DogAPI docs. 
+4. Note that the API will return an HTTP status code of 404 along with a JSON object with info about the error if a request is made for a breed that can't be found.
---
+++ logic.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function watchUserInput() {
+  $("#dog-num-form").submit(e => {
+    e.preventDefault();
+    let userNumInput = $("#num-dog").val();
+    //Pass the number value to getDogImage
+    getDogImage(userNumInput);
+  });
+}
+
+$(function() {
+  console.log("App loaded! Waiting for submit!");
+  watchUserInput();
+});
+
+//Pass numInput, which represents an integer as an argument
+function getDogImage(numInput) {
+  if (numInput < 3) {
+    fetch("https://dog.ceo/api/breeds/image/random/3")
+      .then(response => response.json())
+      .then(responseJson =>  displayResults(responseJson))
+       .catch(error => alert('Something went wrong. Try again later.'));
+  } else if (numInput > 50) {
+    return alert("Please choose a number equal or less than 50");
+  } else {
+    fetch(`https://dog.ceo/api/breeds/image/random/${numInput}`)
+      .then(response => response.json())
+      .then(responseJson => displayResults(responseJson))
+       .catch(error => alert('Something went wrong. Try again later.'));
+  }
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  responseJson.message.forEach(renderedImg => {
+  //replace the existing image with the new one
+  $('.results').append(
+    `<img src="${renderedImg}">`
+  )
+  });
+  //display the results section
+  $('.results').removeClass('hidden');
+}
