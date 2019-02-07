ac0b581de806e26e1c1efb33ecb217b8a8859f8e
Alex Cho <alexsjcho@gmail.com>
2019-02-06 19:04:34 -0800

    updated index.html and logic.js to match the problem asked

--- logic.js
+++ logic.js
@@ -2,26 +2,33 @@
 
 $(function() {
   console.log("App loaded! Waiting for submit!");
-  watchGetDogPhoto();
+  watchGetDogButton();
 });
 
-function watchGetDogPhoto() {
-  $("#get-photo").submit(e => {
+function watchGetDogButton() {
+  $("#get-dog").submit(e => {
     e.preventDefault();
     getDogImage();
   });
 }
 
+function getUserInput() {
+  $("breed-type").submit(e => {
+    e.preventDefault();
+    let someBreed = $("#num-dog").val();
+    getDogImage(getNumInput);
+  });
+}
+
 function getDogImage() {
-  fetch("https://dog.ceo/api/breed/hound/images/random")
+  fetch(`https://dog.ceo/api/breed/${getUserInput}/images/random`)
     .then(response => response.json())
     .then(responseJson => displayResults(responseJson.message))
-    .catch(error => alert("The dogs are alittle shy. Try again later."));
+    .catch(error => alert("Hmmm. Cannot find that breed of dog. Try again."));
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
   $(".results").append(`<img src="${responseJson.message}">`);
-  //display the results section
   $(".results").removeClass("hidden");
 }
